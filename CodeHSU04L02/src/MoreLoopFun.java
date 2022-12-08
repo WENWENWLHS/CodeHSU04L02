b19dc99d5cae7fbec488a448c09713f38904cf0a
WENWENWLHS <113708457+WENWENWLHS@users.noreply.github.com>
2022-12-08 09:38:10 -0800

    Examples of for loops

--- CodeHSU04L02/src/MoreLoopFun.java
+++ CodeHSU04L02/src/MoreLoopFun.java
@@ -7,33 +7,34 @@ public class MoreLoopFun {
          */
 
         //a for loop that counts by ones from 1 to 5
-
+        for(int start =1; start <= 5 ; start++){
+            System.out.println(start);
+        }
 
         //a for loop that prints the powers of two up to 100
         //can you have an infinite for loop?
-
+        System.out.println("Powers of 2");
+        for(int start = 1; start <= 100; start *= 2; ){
+            System.out.println(start + " ");
+        }
 
         //write a for loop that counts by 4s from 5 up to 50
-
+        System.out.println("Counting by 4s");
+        for(int i = 5; i <= 50; i += 4;){
+            System.out.print(i + " ");
+        }
 
         //write a for loop that sums the numbers from 1 to 10
+        System.out.println("Counting the sum");
         int total = 0;
+        for(int i = 1; i <= 10; i++;){
+            total += 1;
+        }
+        System.out.println("The sum of integers 1 through 10 is: " + total);
 
         ////////////////////////////////////////////////////
         //for loops compared to while loops////////////////
         //////////////////////////////////////////////////
-
-        for(int num = 1; num <= 5; num++){
-            System.out.println(num);
-        }
-        System.out.println("Finished!");
-
-        int num = 1;
-        while(num <= 5){
-            System.out.println(num);
-            num++;
-        }
-        System.out.println("Finished!");
 
     }
 }
